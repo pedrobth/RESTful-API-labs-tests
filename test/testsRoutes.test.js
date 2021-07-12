352a068660abe2781e8c89d26f2982f5cadb8c2a
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-12 17:27:45 -0300

    feat: delete /tests route jest tests add

--- test/testsRoutes.test.js
+++ test/testsRoutes.test.js
@@ -43,7 +43,7 @@ describe('UPDATE tests', () => {
       ]);
     
   });
-  it('check if API handle a single valid update on tests', async () => {
+  it('put on /tests route handle a single valid update on tests', async () => {
     const updateResponse = await frisby.put(URL_TESTS, [{
       testName: "contagem de hematocritos",
       testNewName: "contagem hematócritos",
@@ -53,7 +53,7 @@ describe('UPDATE tests', () => {
     const parsedResponse = JSON.parse(body);
     expect(parsedResponse.message).toBe('success');
   });
-  it('check if API handle a valid list to update on tests', async () => {
+  it('put on /tests route handle a valid list to update on tests', async () => {
     const updateResponse = await frisby.put(URL_TESTS, [{
       testName: "contagem de hematocritos",
       testNewName: "contagem hematócritos",
@@ -113,7 +113,7 @@ describe('UPDATE tests', () => {
     }]).expect('status', 400);
     const { body } = updateResponse;
     const parsedResponse = JSON.parse(body);
-    expect(parsedResponse.message).toBe('mandatory fields in wrong format, check inputs and try it again');
+    expect(parsedResponse.message).toBe('some of field data exceeds characters limit');
   });
   it('put on /tests route handle bad inputs -> a huge old test name', async () => {
     const updateResponse = await frisby.put(URL_TESTS, [{
@@ -144,7 +144,7 @@ describe('GET tests', () => {
       TESTS[3].testType, TESTS[3].testName,
     ]);
   });
-  it('route /tests exists and return the expected body', async () => {
+  it('route /tests exists, accept get and return the expected body', async () => {
     const getTestsResponse = await frisby.get(URL_TESTS)
     expect(getTestsResponse.status).not.toBe(404);
     const { body } = getTestsResponse;
@@ -172,7 +172,8 @@ describe('INSERT tests', () => {
     const { body } = postTestsResponse;
     const postTestsParsed = JSON.parse(body);
     expect(postTestsParsed.message).toBe('created successfully with the following ids: ');
-    expect(postTestsParsed.ids).toEqual(expect.arrayContaining([1, 2, 3, 4]));
+    expect(postTestsParsed.ids.length).toBe(4);
+    expect(typeof postTestsParsed.ids[0]).toBe('number');
   });
   it('post on /tests route handle bad inputs -> empty body', async () => {
     const postTestsResponse = await frisby.post(URL_TESTS)
@@ -188,7 +189,7 @@ describe('INSERT tests', () => {
       { testType: TESTS[2].testType, testName: TESTS[2].testName },
       { testType: TESTS[3].testType, testName: TESTS[3].testName },
     );
-    expect(postTestsResponse.status).not.toBe(404);
+    expect(postTestsResponse.status).toBe(400);
     const { body } = postTestsResponse;
     const postTestsParsed = JSON.parse(body);
     expect(postTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
@@ -200,7 +201,7 @@ describe('INSERT tests', () => {
       { testType: 'invalidType', testName: TESTS[2].testName },
       { testType: TESTS[3].testType, testName: TESTS[3].testName },
     );
-    expect(postTestsResponse.status).not.toBe(404);
+    expect(postTestsResponse.status).toBe(400);
     const { body } = postTestsResponse;
     const postTestsParsed = JSON.parse(body);
     expect(postTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
@@ -212,21 +213,116 @@ describe('INSERT tests', () => {
       { testType: '', testName: TESTS[2].testName },
       { testType: TESTS[3].testType, testName: TESTS[3].testName },
     );
-    expect(postTestsResponse.status).not.toBe(404);
+    expect(postTestsResponse.status).toBe(400);
     const { body } = postTestsResponse;
     const postTestsParsed = JSON.parse(body);
     expect(postTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
   });
   it('post on /tests route handle bad inputs -> huge test name', async () => {
-    const postTestsResponse = await frisby.post(URL_TESTS,
+    const postTestsResponse = await frisby.post(URL_TESTS, [
       { testType: TESTS[0].testType, testName: TESTS[0].testName },
       { testType: TESTS[1].testType, testName: LOREM },
       { testType: TESTS[1].testType, testName: TESTS[2].testName },
       { testType: TESTS[3].testType, testName: TESTS[3].testName },
-    );
-    expect(postTestsResponse.status).not.toBe(404);
+    ]);
+    expect(postTestsResponse.status).toBe(400);
     const { body } = postTestsResponse;
     const postTestsParsed = JSON.parse(body);
-    expect(postTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
+    expect(postTestsParsed.message).toBe('some of field data exceeds characters limit');
+  });
+});
+
+describe('DELETE tests', () => {
+  beforeEach(async () => {
+    await connection.execute('DELETE FROM tests_laboratories');
+    await connection.execute('DELETE FROM tests');
+    await connection.execute('ALTER TABLE tests_laboratories AUTO_INCREMENT = 1');
+    await connection.execute('ALTER TABLE tests AUTO_INCREMENT = 1');
+    await connection.execute('INSERT INTO tests (test_type, test_name) VALUES (?, ?), (?, ?), (?, ?), (?, ?)', [
+      TESTS[0].testType, TESTS[0].testName,
+      TESTS[1].testType, TESTS[1].testName,
+      TESTS[2].testType, TESTS[2].testName,
+      TESTS[3].testType, TESTS[3].testName,
+    ]);
+    await connection.execute('INSERT INTO tests_laboratories (laboratory_id, test_id) VALUES(?, ?), (?, ?), (?, ?), (?, ?), (?, ?), (?, ?)', [
+      RELATIONS[0].laboratoryId, RELATIONS[0].testId,
+      RELATIONS[1].laboratoryId, RELATIONS[1].testId,
+      RELATIONS[2].laboratoryId, RELATIONS[2].testId,
+      RELATIONS[3].laboratoryId, RELATIONS[3].testId,
+      RELATIONS[4].laboratoryId, RELATIONS[4].testId,
+      RELATIONS[5].laboratoryId, RELATIONS[5].testId,
+    ]);
+  });
+  it('delete on /tests route remove a single test successfully', async () => {
+    const deleteTestsResponse = await frisby.delete(URL_TESTS, [
+      { testName: TESTS[1].testName },
+    ]).expect('status', 200);
+    const { body } = deleteTestsResponse;
+    const deleteTestsParsed = JSON.parse(body);
+    expect(deleteTestsParsed.message).toBe('deleted with relations removed successfully');
+  });
+  it('delete on /tests route remove a tests lists successfully', async () => {
+    const deleteTestsResponse = await frisby.delete(URL_TESTS, [
+      { testName: TESTS[1].testName },
+    ]).expect('status', 200);
+    const { body } = deleteTestsResponse;
+    const deleteTestsParsed = JSON.parse(body);
+    expect(deleteTestsParsed.message).toBe('deleted with relations removed successfully');
+  });
+  it('delete on /tests route handle bad inputs -> empty body', async () => {
+    const deleteTestsResponse = await frisby.delete(URL_TESTS)
+    expect(deleteTestsResponse.status).toBe(400);
+    const { body } = deleteTestsResponse;
+    const deleteTestsParsed = JSON.parse(body);
+    expect(deleteTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
+  });
+  it('delete on /tests route handle bad inputs -> tests out of a list', async () => {
+    const deleteTestsResponse = await frisby.delete(URL_TESTS,
+      { testName: TESTS[0].testName },
+      { testName: TESTS[1].testName },
+      { testName: TESTS[2].testName },
+      { testName: TESTS[3].testName },
+    );
+    expect(deleteTestsResponse.status).toBe(400);
+    const { body } = deleteTestsResponse;
+    const deleteTestsParsed = JSON.parse(body);
+    expect(deleteTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
+  });
+  it('delete on /tests route handle bad inputs -> invalid test type', async () => {
+    const deleteTestsResponse = await frisby.delete(URL_TESTS,
+      { testName: TESTS[0].testName },
+      { testName: TESTS[1].testName },
+      { testName: TESTS[2].testName },
+      { testName: TESTS[3].testName },
+    );
+    expect(deleteTestsResponse.status).toBe(400);
+    const { body } = deleteTestsResponse;
+    const deleteTestsParsed = JSON.parse(body);
+    expect(deleteTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
+  });
+  it('delete on /tests route handle bad inputs -> empty test type', async () => {
+    const deleteTestsResponse = await frisby.delete(URL_TESTS,
+      { testName: TESTS[0].testName },
+      { testName: TESTS[1].testName },
+      { testName: TESTS[2].testName },
+      { testName: TESTS[3].testName },
+    );
+    expect(deleteTestsResponse.status).toBe(400);
+    const { body } = deleteTestsResponse;
+    const deleteTestsParsed = JSON.parse(body);
+    expect(deleteTestsParsed.message).toBe('mandatory fields missing or in wrong format, check inputs and try it again');
+  });
+  it('delete on /tests route handle bad inputs -> huge test name', async () => {
+    const deleteTestsResponse = await frisby.delete(URL_TESTS, [
+      { testName: TESTS[0].testName },
+      { testName: LOREM },
+      { testName: TESTS[2].testName },
+      { testName: TESTS[3].testName },
+    ]);
+    expect(deleteTestsResponse.status).toBe(208);
+    const { body } = deleteTestsResponse;
+    const deleteTestsParsed = JSON.parse(body);
+    expect(deleteTestsParsed.message).toBe('at least one request fail');
+    expect(deleteTestsParsed.failRequests[0].testName).toBe(LOREM);
   });
 });
