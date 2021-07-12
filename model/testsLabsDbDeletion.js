55bd89f0d02af9b12fcf61fb3a5c7d58bf4f2fb0
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-12 17:22:36 -0300

    fix: testsLabsDbDeletion return correct value

--- model/testsLabsDbDeletion.js
+++ model/testsLabsDbDeletion.js
@@ -3,9 +3,13 @@ const connection = require('./connection');
 const testsLabsDbDeletion = async (tests) => {
   try {
     const response = tests.map((test) => connection
-      .execute('DELETE FROM tests_laboratories WHERE test_id=?', [test.id]));
+      .execute('DELETE FROM tests_laboratories WHERE test_id=('
+        +'SELECT id FROM tests WHERE test_name=?'
+      +')', [test.testName]));
     const deletionList = await Promise.all(response)
-      .then((resp) => resp.map((e) => e[0].changedRows));
+      .then((resp) => resp
+        .map((e) =>  e[0].affectedRows)
+      );
     return deletionList;
   } catch (err) {
     console.log('error at testsLabsDbDeletion: ', err);
