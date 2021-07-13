aa23f2210d1bd699572f45f3f65b3d593841d67e
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-13 05:33:36 -0300

    fix: labsDeletion import

--- services/index.js
+++ services/index.js
@@ -2,6 +2,7 @@ const allActiveLabs = require('./allActiveLabs');
 const allActiveTests = require('./allActiveTests');
 const associationDeletion = require('./associationDeletion');
 const associationInsertion = require('./associationInsertion');
+const labsDeletion = require('./labsDeletion');
 const labsInsertion = require('./labsInsertion');
 const testsDeletion = require('./testsDeletion');
 const testsInsertion = require('./testsInsertion');
@@ -13,6 +14,7 @@ module.exports = {
   associationDeletion,
   associationInsertion,
   allActiveTests,
+  labsDeletion,
   labsInsertion,
   testsDeletion,
   testsInsertion,
