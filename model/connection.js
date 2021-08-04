76ac3912819abbbe1fe9294393e78baa20f2f354
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-04 12:52:01 -0300

    fix: misstype environment variable

--- model/connection.js
+++ model/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createPool({
   host: process.env.HOSTNAME,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.DB.DEVELOPMENT || process.env.DB,
+  database: process.env.DB_DEVELOPMENT || process.env.DB,
 });
 
 module.exports = connection;
