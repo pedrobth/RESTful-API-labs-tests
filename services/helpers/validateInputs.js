1c9281e8785d8f382c30287a643b1dba5e861087
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-05 20:40:33 -0300

    fix: test inputs to big to fit in db

--- services/helpers/validateInputs.js
+++ services/helpers/validateInputs.js
@@ -3,7 +3,9 @@ const validateInputs = (requiredFields, inputs) => {
   if (!inputs.every((i) => Object.values(i))) return false;
   if (!requiredFields.every((field) => inputs
     .every((i) => i[field]))) return false;
-  return true;
+  inputsList = inputs.map((input) => Object.values(input))
+  if (inputsList.flat().some((input) => input.length > 255)) return false;
+    return true;
 };
 
 module.exports = validateInputs;
