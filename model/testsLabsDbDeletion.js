f539c6469aab2444f282aa141c5b784dbaacdd3a
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-07 14:51:37 -0300

    refactor: sql syntax uses test id for deletion

--- model/testsLabsDbDeletion.js
+++ model/testsLabsDbDeletion.js
@@ -2,10 +2,10 @@ const connection = require('./connection');
 
 const testsLabsDbDeletion = async (tests) => {
   try {
+    console.log(tests)
     const response = tests.map((test) => connection
-      .execute('DELETE FROM tests_laboratories WHERE test_id=('
-        +'SELECT id FROM tests WHERE test_name=?'
-      +')', [test.testName]));
+      .execute('DELETE FROM tests_laboratories WHERE test_id=?',
+        [test.testId]));
     const deletionList = await Promise.all(response)
       .then((resp) => resp
         .map((e) =>  e[0].affectedRows)
