6e04fe6b17d5c64207fa2e127df94e9bb1be6ff8
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-08 11:11:25 -0300

    refactor: association deletion model use lab id

--- model/removeAssociation.js
+++ model/removeAssociation.js
@@ -1,14 +1,13 @@
 const connection = require('./connection');
 
-const associateTestsToLab = async (testsList, labName) => {
+const associateTestsToLab = async (testsList, labId) => {
   try {
+    console.log('REMOVE ASSOCIATION: ', testsList, labId)
     const response = testsList.map((test) => connection
       .execute('DELETE FROM tests_laboratories '
         +'WHERE test_id=('
           +'SELECT id FROM tests WHERE test_name=? AND active=true'
-        +') AND laboratory_id=('
-          +'SELECT id FROM laboratories WHERE lab_name=? AND active=true'
-        +')', [test.testName, labName]));
+        +') AND laboratory_id=?', [test.testName, labId]));
     const insertedList = await Promise.all(response)
       .then((resp) => resp.map((e) => e[0].affectedRows));
     return insertedList;
