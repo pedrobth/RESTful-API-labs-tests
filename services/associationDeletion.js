eb0c4ac52388b68c07e356a01411d99f3b642df3
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-08 11:05:40 -0300

    refactor: association deletion service use lab id

--- services/associationDeletion.js
+++ services/associationDeletion.js
@@ -2,11 +2,12 @@ const { removeAssociation } = require('../model');
 const validateInputs = require('./helpers/validateInputs');
 const statusMessages = require('./dictionary/statusMessages');
 
-const associationDeletion = async (body, labName) => {
+const associationDeletion = async (body, labId) => {
   try {
+    // I did something unusual on this application. Update, delete and insert many was an extra feature. Those requisitions take the id via body instead of prarams. Since It has a single route to update one and many laboratories, this approach is easier to maintain. In that specific case (deletion) the application still working with tests name, but for exercise purpose it would be good to work with subqueries.
     const requiredFields = ['testName'];
     if (!validateInputs(requiredFields, body)) return statusMessages.missingFields;
-    const associationRes = await removeAssociation(body, labName);
+    const associationRes = await removeAssociation(body, labId);
     if (associationRes.code) return statusMessages[`${associationRes.code}`];
     const allUpdated = associationRes
       .find((insertion) => insertion === 0);
