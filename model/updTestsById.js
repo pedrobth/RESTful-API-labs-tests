ff006e8529a7632222f366fc447fd812f378df82
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-09 12:50:50 -0300

    style: no console.log on updTestsById

--- model/updTestsById.js
+++ model/updTestsById.js
@@ -2,7 +2,6 @@ const connection = require('./connection');
 
 const updTestsById = async (testsList) => {
   try {
-    console.log(testsList)
     const response = testsList.map((test) => connection
       .execute('UPDATE tests SET tests.test_name=?, tests.test_type=? '
         +'WHERE id=?',
