3092adc7cdc4bd34a6ce5a32581fd600fff66001
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-09 12:47:50 -0300

    refactor: model assoc. insertion works with id

--- model/associateTestsToLab.js
+++ model/associateTestsToLab.js
@@ -1,18 +1,18 @@
 const connection = require('./connection');
 
-const associateTestsToLab = async (testsList, labName) => {
+const associateTestsToLab = async (testsList, labId) => {
   try {
     const response = testsList.map((test) => connection
       .execute('INSERT INTO tests_laboratories (test_id, laboratory_id) '
       + 'VALUES('
         + '(SELECT id FROM tests WHERE test_name=? AND active = true), '
-        + '(SELECT id FROM laboratories WHERE lab_name=? AND active = true)'
-      + ')', [test.testName, labName]));
+        + '?'
+      + ')', [test.testName, labId]));
     const insertedList = await Promise.all(response)
       .then((resp) => resp.map((e) => e[0].affectedRows));
     return insertedList;
   } catch (err) {
-    // console.log('error at associateTestsToLab: ', err);
+    console.log('error at associateTestsToLab: ', err);
     return err;
   }
 };
