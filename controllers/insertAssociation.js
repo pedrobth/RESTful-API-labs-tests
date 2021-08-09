41c5d4d8cfab9e3930da34f3ae96e0c791a6dffb
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-09 12:37:53 -0300

    refactor: controller use id on assoc. insertion

--- controllers/insertAssociation.js
+++ controllers/insertAssociation.js
@@ -3,10 +3,10 @@ const { associationInsertion } = require('../services');
 
 const insertAssociation = Router();
 
-insertAssociation.post('/:labName', async (req, res, next) => {
+insertAssociation.post('/:labId', async (req, res, next) => {
   try {
     const { body, params } = req;
-    const insertionResponse = await associationInsertion(body, params.labName);
+    const insertionResponse = await associationInsertion(body, params.labId);
     if (insertionResponse.err) return next(insertionResponse);
     const { message, status } = insertionResponse;
     return res.status(status).json({ message });
