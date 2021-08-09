9b2aab2c7f340ffa32c8bd7e0307c5a1ed625a60
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-09 12:40:25 -0300

    refactor: service use id on assoc. insertion

--- services/associationInsertion.js
+++ services/associationInsertion.js
@@ -1,12 +1,17 @@
-const { associateTestsToLab } = require('../model');
+const { associateTestsToLab, getLabsById, getTestsByName } = require('../model');
 const validateInputs = require('./helpers/validateInputs');
 const statusMessages = require('./dictionary/statusMessages');
 
-const associationInsertion = async (body, labName) => {
+const associationInsertion = async (body, labId) => {
   try {
     const requiredFields = ['testName'];
     if (!validateInputs(requiredFields, body)) return statusMessages.missingFields;
-    const associationRes = await associateTestsToLab(body, labName);
+    const testsSearch = await getTestsByName(body);
+    const someTestNotInDb = testsSearch.some((t) => t === undefined)
+    const lab = await getLabsById([{ labId: labId }]);
+    if (someTestNotInDb || lab[0] === undefined) return statusMessages.idNotInDatabase
+    
+    const associationRes = await associateTestsToLab(body, labId);
     if (associationRes.code) return statusMessages[`${associationRes.code}`];
     return statusMessages.associationCreated;
   } catch (err) {
