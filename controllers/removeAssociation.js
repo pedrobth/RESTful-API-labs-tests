a74c38453cc0f2f859d27ebc525c9f945e3a7d93
Pedro Risso <pmerchiorattor@gmail.com>
2021-09-21 18:56:48 -0300

    style: comment remove

--- controllers/removeAssociation.js
+++ controllers/removeAssociation.js
@@ -3,7 +3,6 @@ const { associationDeletion } = require('../services');
 
 const removeAssociation = Router();
 
-// I did something unusual on this application. Update, delete and insert many was an extra feature. Those requisitions take the id via body instead of prarams. Since It has a single route to update one and many laboratories, this approach is easier to maintain.
 removeAssociation.delete('/:labId', async (req, res, next) => {
   try {
     const { body, params } = req;
