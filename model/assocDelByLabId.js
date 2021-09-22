4344ec9157c2d5836849de9d222a91325589446c
Pedro Risso <pmerchiorattor@gmail.com>
2021-09-22 11:05:42 -0300

    refactor: REST acrchitecture compliance del 1 id

--- model/assocDelByLabId.js
+++ model/assocDelByLabId.js
@@ -1,13 +1,12 @@
 const connection = require('./connection');
 
-const assocDelByLabId = async (labsList) => {
+const assocDelByLabId = async (labId) => {
   try {
-    const response = labsList.map((lab) => connection
-      .execute('DELETE FROM tests_laboratories '
-        +'WHERE laboratory_id=?', [lab.labId]));
-    const insertedList = await Promise.all(response)
-      .then((resp) => resp.map((e) => e[0].affectedRows));
-    return insertedList;
+    const response = connection.execute('DELETE FROM tests_laboratories '
+        +'WHERE laboratory_id=?', labId);
+    const deleted = await response
+    console.log('asssocDelBy:', deleted)
+    return deleted[0];
   } catch (err) {
     console.log('error at assocDelByLabId: ', err);
     return err;
