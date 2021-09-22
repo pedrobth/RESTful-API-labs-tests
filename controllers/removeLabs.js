ee1f38105e56144a3931d1f4d54744438a315a94
Pedro Risso <pmerchiorattor@gmail.com>
2021-09-22 11:00:19 -0300

    refactor: removeLabs RESTFULL architecture

--- controllers/removeLabs.js
+++ controllers/removeLabs.js
@@ -3,11 +3,11 @@ const { labsDeletion } = require('../services');
 
 const removeLabs = Router();
 
-removeLabs.delete('/', async (req, res, next) => {
+removeLabs.delete('/:labId', async (req, res, next) => {
   try {
-    // I did something unusual on this application. Update, delete and insert many was an extra feature. Those requisitions take the id via body instead of prarams. Since It has a single route to update one and many laboratories, this approach is easier to maintain..
-    const { body } = req;
-    const deletionResponse = await labsDeletion(body);
+    const { params } = req;
+    const { labId } = params;
+    const deletionResponse = await labsDeletion(labId);
     if (deletionResponse.err) return next(deletionResponse);
     const { message, status } = deletionResponse;
     return res.status(status)
