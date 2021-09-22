bf35826e90ecab0af2e5669e84d818b23042961a
Pedro Risso <pmerchiorattor@gmail.com>
2021-09-22 11:04:14 -0300

    refactor: feedback-REST acrchitecture compliance

--- services/dictionary/statusMessages.js
+++ services/dictionary/statusMessages.js
@@ -32,6 +32,9 @@ const statusMessasges = {
   idNotInDatabase: {
     status: 400, message: 'At least one Id in your request cannot be found'
   },
+  labDeletedNoAssiciationFound: {
+    status: 200, message: 'lab removed. WARNING: "could not find any relation with tests"'
+  },
   missingFields: {
     status: 400, message: 'mandatory fields missing or in wrong format, check inputs and try it again',
   },
